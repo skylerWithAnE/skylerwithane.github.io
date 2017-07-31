3616979bdf189b56b15ae3dd2dcb3720a759b0b1
Skyler Evans <skylerWithAnE@bitbucket.org>
2017-07-31 15:46:28 -0500

    show current transform

--- js/simpleobjecttracking.js
+++ js/simpleobjecttracking.js
@@ -144,7 +144,9 @@ var World = {
             var cssDivLeft = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
             var cssDivRight = " style='display: table-cell;vertical-align: middle; text-align: left;'";
             document.getElementById('loadingMessage').innerHTML =
-                "<div" + cssDivLeft + ">Scan R2(web: 2):</div>" +
+                "<div" + cssDivLeft + ">Position: " + World.tX.toString(); ", " + World.tY.toString(); ", " + World.tZ.toString(); + 
+                "<br>Rotation: " + World.rX.toString(); ", " + World.rY.toString(); ", " + World.rZ.toString(); + 
+                "<br>Scale: "+ World.tX.toString(); ", " + World.tY.toString(); ", " + World.tZ.toString(); + "</div>" +
                 "<div" + cssDivRight + "><img src='assets/r2_thumb.png'></img></div>";
         }
     }
