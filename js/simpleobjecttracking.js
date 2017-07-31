265d9d42e9fec284f60415b470c211fb60beb1aa
Skyler Evans <skylerWithAnE@bitbucket.org>
2017-07-31 13:45:30 -0500

    undo test code

--- js/simpleobjecttracking.js
+++ js/simpleobjecttracking.js
@@ -5,124 +5,122 @@ var World = {
 
     init: function initFn() {
         var x = Document.URL;
-        console.log("x");
-        //World.createOccluder();
-        //World.createCones();
-        //World.createTracker();
-    }
-//     },
+        World.createOccluder();
+        World.createCones();
+        World.createTracker();
+    },
 
-//     createOccluder: function createOccluderFn() {
-//         var occluderScale = 0.00;
+    createOccluder: function createOccluderFn() {
+        var occluderScale = 0.00;
 
-//         this.firetruckOccluder = new AR.Occluder("assets/firetruck_occluder.wt3", {
-//             onLoaded: this.loadingStep,
-//             scale: {
-//                 x: occluderScale,
-//                 y: occluderScale,
-//                 z: occluderScale
-//             },
-//             translate: {
-//                 x: -0.25,
-//                 z: -0.3
-//             },
-//             rotate: {
-//                 x: 180
-//             }
-//         });
-//         World.drawables.push(this.firetruckOccluder);
-//     },
+        this.firetruckOccluder = new AR.Occluder("assets/firetruck_occluder.wt3", {
+            onLoaded: this.loadingStep,
+            scale: {
+                x: occluderScale,
+                y: occluderScale,
+                z: occluderScale
+            },
+            translate: {
+                x: -0.25,
+                z: -0.3
+            },
+            rotate: {
+                x: 180
+            }
+        });
+        World.drawables.push(this.firetruckOccluder);
+    },
 
-//     createCones: function createConesFn() {
-//         var coneDistance = 1.0;
+    createCones: function createConesFn() {
+        var coneDistance = 1.0;
 
-// //        var frontLeftCone = World.getCone(-coneDistance, 0.0, World.occluderCenterZ + coneDistance);
-// //        World.drawables.push(frontLeftCone);
-// //
-// //        var backLeftCone = World.getCone( coneDistance, 0.0, World.occluderCenterZ + coneDistance);
-// //        World.drawables.push(backLeftCone);
+//        var frontLeftCone = World.getCone(-coneDistance, 0.0, World.occluderCenterZ + coneDistance);
+//        World.drawables.push(frontLeftCone);
+//
+//        var backLeftCone = World.getCone( coneDistance, 0.0, World.occluderCenterZ + coneDistance);
+//        World.drawables.push(backLeftCone);
 
-//         var backRightCone = World.getCone( 1.0, 0.0, World.occluderCenterZ);
-//         World.drawables.push(backRightCone);
+        var backRightCone = World.getCone( 1.0, 0.0, World.occluderCenterZ);
+        World.drawables.push(backRightCone);
 
-// //        var frontRightCone = World.getCone(-coneDistance, 0.0, World.occluderCenterZ - coneDistance);
-// //        World.drawables.push(frontRightCone);
-//     },
+//        var frontRightCone = World.getCone(-coneDistance, 0.0, World.occluderCenterZ - coneDistance);
+//        World.drawables.push(frontRightCone);
+    },
 
-//     getCone: function getConeFn(positionX, positionY, positionZ) {
-//         var coneScale = 0.01;
+    getCone: function getConeFn(positionX, positionY, positionZ) {
+        var coneScale = 0.01;
 
-//         return new AR.Model("assets/box5.wt3", {
-//             scale: {
-//                 x: coneScale,
-//                 y: 0.03,
-//                 z: coneScale
-//             },
-//             translate: {
-//                 x: positionX,
-//                 y: positionY,
-//                 z: positionZ
-//             },
-//             rotate: {   
-//                 x: 0
-//             }
-//         });
-//     },
+        return new AR.Model("assets/box5.wt3", {
+            scale: {
+                x: coneScale,
+                y: 0.03,
+                z: coneScale
+            },
+            translate: {
+                x: positionX,
+                y: positionY,
+                z: positionZ
+            },
+            rotate: {   
+                x: 0
+            }
+        });
+    },
 
-//     createTracker: function createTrackerFn() {
-//         this.targetCollectionResource = new AR.TargetCollectionResource("assets/tracker3.wto", {
-//         });
+    createTracker: function createTrackerFn() {
+        this.targetCollectionResource = new AR.TargetCollectionResource("assets/tracker3.wto", {
+        });
 
-//         this.tracker = new AR.ObjectTracker(this.targetCollectionResource, {
-//             onError: function(errorMessage) {
-//                 alert(errorMessage);
-//             }
-//         });
+        this.tracker = new AR.ObjectTracker(this.targetCollectionResource, {
+            onError: function(errorMessage) {
+                alert(errorMessage);
+            }
+        });
         
-//         this.objectTrackable = new AR.ObjectTrackable(this.tracker, "*", {
-//             drawables: {
-//                 cam: World.drawables
-//             },
-//             onObjectRecognized: this.objectRecognized,
-//             onObjectLost: this.objectLost,
-//             onError: function(errorMessage) {
-//                 alert(errorMessage);
-//             }
-//         });
-//     },
+        this.objectTrackable = new AR.ObjectTrackable(this.tracker, "*", {
+            drawables: {
+                cam: World.drawables
+            },
+            onObjectRecognized: this.objectRecognized,
+            onObjectLost: this.objectLost,
+            onError: function(errorMessage) {
+                alert(errorMessage);
+            }
+        });
+    },
 
-//     objectRecognized: function objectRecognizedFn() {
-//         World.removeLoadingBar();
-//         World.setAugmentationsEnabled(true);
-//     },
+    objectRecognized: function objectRecognizedFn() {
+        World.removeLoadingBar();
+        World.setAugmentationsEnabled(true);
+    },
 
-//     objectLost: function objectLostFn() {
-//         World.setAugmentationsEnabled(false);
-//     },
+    objectLost: function objectLostFn() {
+        World.setAugmentationsEnabled(false);
+    },
 
-//     setAugmentationsEnabled: function setAugmentationsEnabledFn(enabled) {
-//         for (var i = 0; i < World.drawables.length; i++) {
-//             World.drawables[i].enabled = enabled;
-//         }
-//     },
+    setAugmentationsEnabled: function setAugmentationsEnabledFn(enabled) {
+        for (var i = 0; i < World.drawables.length; i++) {
+            World.drawables[i].enabled = enabled;
+        }
+    },
 
-//     removeLoadingBar: function removeLoadingBarFn() {
-//         if (!World.loaded && World.firetruckOccluder.isLoaded()) {
-//             var e = document.getElementById('loadingMessage');
-//             e.parentElement.removeChild(e);
-//             World.loaded = true;
-//         }
-//     },
+    removeLoadingBar: function removeLoadingBarFn() {
+        if (!World.loaded && World.firetruckOccluder.isLoaded()) {
+            var e = document.getElementById('loadingMessage');
+            e.parentElement.removeChild(e);
+            World.loaded = true;
+        }
+    },
 
-//     loadingStep: function loadingStepFn() {
-//         if (World.firetruckOccluder.isLoaded()) {
-//             var cssDivLeft = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
-//             var cssDivRight = " style='display: table-cell;vertical-align: middle; text-align: left;'";
-//             document.getElementById('loadingMessage').innerHTML =
-//                 "<div" + cssDivLeft + ">Scan R2(web: 2):</div>" +
-//                 "<div" + cssDivRight + "><img src='assets/r2_thumb.png'></img></div>";
-//         }
-//     }
+    loadingStep: function loadingStepFn() {
+        if (World.firetruckOccluder.isLoaded()) {
+            var cssDivLeft = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
+            var cssDivRight = " style='display: table-cell;vertical-align: middle; text-align: left;'";
+            document.getElementById('loadingMessage').innerHTML =
+                "<div" + cssDivLeft + ">Scan R2(web: 2):</div>" +
+                "<div" + cssDivRight + "><img src='assets/r2_thumb.png'></img></div>";
+        }
+    }
 };
 
 World.init();
